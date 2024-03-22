479d7c86f9f579e528626e8837740051319c8723
Mboizi Eric <ericb55458@gmail.com>
2024-03-22 08:18:39 +0000

    ‘database’

--- example/src/dnn.js
+++ example/src/dnn.js
@@ -1,20 +1,61 @@
-function registerDNN(input){
+import { connection, closeDatabaseConnection } from "./database/connection.js";
+
+async function registerDNN(input){
+    return new Promise((resolve, reject) => {
+        const db = connection();
+    
+        db.run('INSERT INTO Dnn (name, creator, description, source_code) VALUES (?, ?, ?, ?)', [input.name, input.creator, input.description, input.source_code], (err) => {
+          closeDatabaseConnection(db);
+    
+          if (err) {
+            console.error(`Error in vote: ${err.message}`);
+            reject(err);
+          } else {
+            resolve();
+          }
+        });
+      });
+}
+
+async function registerDrawing(name, dnn){
+    return new Promise((resolve, reject) => {
+        const db = connection();
+    
+        db.run('INSERT INTO Drawing (name, dnn) VALUES (?, ?)', [name, dnn], (err) => {
+          closeDatabaseConnection(db);
+    
+          if (err) {
+            console.error(`Error in vote: ${err.message}`);
+            reject(err);
+          } else {
+            resolve();
+          }
+        });
+      });
+}
+
+async function registerFraction(name, drawing){
+    return new Promise((resolve, reject) => {
+        const db = connection();
+    
+        db.run('INSERT INTO Fraction (name, drawing) VALUES (?, ?)', [name, drawing], (err) => {
+          closeDatabaseConnection(db);
+    
+          if (err) {
+            console.error(`Error in vote: ${err.message}`);
+            reject(err);
+          } else {
+            resolve();
+          }
+        });
+      });
+}
+
+async function transferDrawing(to){
 
 }
 
-function registerDrawing(dnn, drawing){
-
-}
-
-function registerFraction(drawing, fraction){
-
-}
-
-function transferDrawing(to){
-
-}
-
-function transferFraction(to){
+async function transferFraction(to){
 
 }
 
