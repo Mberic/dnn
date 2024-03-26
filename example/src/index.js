f73d0389dcac2b43dfd5f6ec7b921b6ffd4b19ed
Mboizi Eric <ericb55458@gmail.com>
2024-03-26 07:40:50 +0000

    p5 package

--- example/src/index.js
+++ example/src/index.js
@@ -2,6 +2,31 @@ import { ethers } from "ethers";
 import puppeteer from 'puppeteer';
 
 import { registerDNN, registerDrawing, registerFraction, transferDrawing, transferFraction } from "./dnn.js";
+import fs from 'fs';
+
+function readFileContents(filePath) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, 'utf8', (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data);
+      }
+    });
+  });
+}
+
+const filePath = 'node_modules/p5/lib/p5.js';
+let p5Module;
+
+readFileContents(filePath)
+  .then(contents => {
+    p5Module = contents;
+    // Use the file contents as needed
+  })
+  .catch(error => {
+    console.error('Error reading file:', error);
+  });
 
 const rollup_server = process.env.ROLLUP_HTTP_SERVER_URL;
 console.log("HTTP rollup_server url is " + rollup_server);
@@ -68,7 +93,7 @@ async function action_proxy(payload, address){
           <meta charset="UTF-8">
           <meta name="viewport" content="width=device-width, initial-scale=1.0">
           <title>p5.js Code</title>
-          <script src="../node_modules/p5/lib/p5.js"></script>
+          <script>${p5Module}</script>
           <script>${jsCode}</script>
         </head>
         <body>
